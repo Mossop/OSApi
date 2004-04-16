a904385dd9cb57b231ade8f1f88d2c00bfc0e1bc
Dave Townsend <dtownsend@oxymoronical.com>
2004-04-16 13:53:59 +0000

    Data is now managed better by a pointer help in the java object. Pointers are also held in a linked list to try to make cleanup easy on a bad exit state.

--- native/NativePam.cpp
+++ native/NativePam.cpp
@@ -1,58 +1,92 @@
 #include <string.h>
-#include <map>
 #include "com_blueprintit_security_pam_Pam.h"
 #include "pamcalls.cpp"
 
 using namespace std;
 
-map<int, pam_handle_t*> handles;
-
-extern "C" jint JNI_OnLoad(JavaVM *vm, void *reserved)
+struct native_data
 {
-	open_pam();
-	return JNI_VERSION_1_4;
-}
-
-extern "C" void JNI_OnUnload(JavaVM *vm, void *reserved)
-{
-	close_pam();
-}
-
-int find_id(JNIEnv *env, jobject obj)
-{
-	jclass clsPam = env->GetObjectClass(obj);
-	jmethodID methGetID = env->GetMethodID(clsPam,"getID","()I");
-	return env->CallIntMethod(obj,methGetID);
-}
-
-pam_handle_t* fetch_handle(JNIEnv *env, jobject obj)
-{
-	int id = find_id(env,obj);
-	pam_handle_t* handle = handles[id];
-	return handle;
-}
-
-void set_handle(JNIEnv *env, jobject obj, pam_handle_t *handle)
-{
-	int id = find_id(env,obj);
-	handles.insert(make_pair(id,handle));
-}
-
-void clear_handle(JNIEnv *env, jobject obj)
-{
-	int id = find_id(env,obj);
-	handles.erase(id);
-}
-
-struct pam_data
-{
-	jobject callback;
+	jobject Pam;
 	JNIEnv *env;
+	jobject callback;
+	pam_handle_t *pamhandle;
+	struct native_data *next;
+	struct native_data *prev;
 };
+
+struct native_data *first = NULL;
+
+struct native_data *create_data(JNIEnv *env, jobject obj)
+{
+	struct native_data *data = new (struct native_data);
+	data->pamhandle=NULL;
+
+	data->env=env;
+	data->Pam=env->NewGlobalRef(obj);
+
+	data->next=first;
+	data->prev=NULL;
+
+	if (first!=NULL)
+		first->prev=data;
+	first=data;
+
+	jmethodID methSetData = env->GetMethodID(env->GetObjectClass(obj),"setNativeData","([B)V");
+	jbyteArray jaryData = env->NewByteArray(sizeof data);
+	jboolean iscopy;
+	jbyte *pinned = env->GetByteArrayElements(jaryData,&iscopy);
+
+	*((struct native_data**)pinned)=data;
+
+	env->ReleaseByteArrayElements(jaryData,pinned,0);
+
+	env->CallVoidMethod(obj,methSetData,jaryData);
+
+	return data;
+}
+
+struct native_data *get_data(JNIEnv *env, jobject obj)
+{
+	jmethodID methGetData = env->GetMethodID(env->GetObjectClass(obj),"getNativeData","()[B");
+	jbyteArray jaryData = (jbyteArray)env->CallObjectMethod(obj,methGetData);
+	jboolean iscopy;
+	jbyte *pinned = env->GetByteArrayElements(jaryData,&iscopy);
+
+	struct native_data *data = *((struct native_data**)pinned);
+
+	env->ReleaseByteArrayElements(jaryData,pinned,0);
+
+	return data;
+}
+
+void release_data(struct native_data *data)
+{
+	if (data->next!=NULL)
+		data->next->prev=data->prev;
+	if (data->prev!=NULL)
+		data->prev->next=data->next;
+	if (data==first)
+		first=data->next;
+
+	if (data->pamhandle!=NULL)
+	{
+		call_pam_end(data->pamhandle,0);
+		data->pamhandle=NULL;
+	}
+
+	jmethodID methSetData = data->env->GetMethodID(data->env->GetObjectClass(data->Pam),"setNativeData","([B)V");
+	data->env->CallVoidMethod(data->Pam,methSetData,NULL);
+
+	data->env->DeleteGlobalRef(data->Pam);
+	data->Pam=NULL;
+	data->env=NULL;
+
+	delete data;
+}
 
 static int pam_converser(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr)
 {
-	struct pam_data *data = (struct pam_data*)appdata_ptr;
+	struct native_data *data = (struct native_data*)appdata_ptr;
 	JNIEnv *env = data->env;
 
 	jclass clsPamMessage = env->FindClass("com/blueprintit/security/pam/PamMessage");
@@ -98,23 +132,34 @@ static int pam_converser(int num_msg, const struct pam_message **msg, struct pam
 	return PAM_SUCCESS;
 }
 
+extern "C" jint JNI_OnLoad(JavaVM *vm, void *reserved)
+{
+	open_pam();
+	return JNI_VERSION_1_4;
+}
+
+extern "C" void JNI_OnUnload(JavaVM *vm, void *reserved)
+{
+	while (first!=NULL)
+		release_data(first);
+	close_pam();
+}
+
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_start
- * Signature: (Ljava/lang/String;Ljava/lang/String;Lcom/blueprintit/security/pam/PamCallback;I)I
+ * Signature: (Ljava/lang/String;Ljava/lang/String;Lcom/blueprintit/security/pam/PamCallback;)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1start
   (JNIEnv *env, jobject obj, jstring service, jstring user, jobject callback)
 {
+	struct native_data *data = create_data(env,obj);
+
 	const char *pam_serv = env->GetStringUTFChars(service,0);
 	const char *pam_user = env->GetStringUTFChars(user,0);
 
 	pam_handle_t *handle;
-	jobject globalref = env->NewGlobalRef(callback);
-
-	struct pam_data *data = new struct pam_data;
-	data->callback=globalref;
-	data->env=env;
+	data->callback = env->NewGlobalRef(callback);
 
 	struct pam_conv *conv = new struct pam_conv;
 	conv->conv=&pam_converser;
@@ -123,7 +168,11 @@ JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1start
 	int status = call_pam_start(strdup(pam_serv),strdup(pam_user),conv,&handle);
 	if (status==PAM_SUCCESS)
 	{
-		set_handle(env,obj,handle);
+		data->pamhandle=handle;
+	}
+	else
+	{
+		release_data(data);
 	}
 
 	env->ReleaseStringUTFChars(service,pam_serv);
@@ -135,98 +184,106 @@ JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1start
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_end
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1end
   (JNIEnv *env, jobject obj, jint pam_status)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
-	int status = call_pam_end(handle,pam_status);
-	clear_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	int status = call_pam_end(data->pamhandle,pam_status);
+	data->pamhandle=NULL;
+	release_data(data);
 	return status;
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_authenticate
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1authenticate
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_authenticate(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_setcred
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1setcred
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_setcred(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_acct_mgmt
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1acct_1mgmt
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_acct_mgmt(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_open_session
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1open_1session
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_open_session(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_close_session
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1close_1session
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_close_session(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_chauthtok
- * Signature: (II)I
+ * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1chauthtok
   (JNIEnv *env, jobject obj, jint flags)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	return call_pam_chauthtok(handle,flags);
 }
 
 /*
  * Class:     com_blueprintit_security_pam_Pam
  * Method:    call_pam_strerror
- * Signature: (II)Ljava/lang/String;
+ * Signature: (I)Ljava/lang/String;
  */
 JNIEXPORT jstring JNICALL Java_com_blueprintit_security_pam_Pam_call_1pam_1strerror
   (JNIEnv *env, jobject obj, jint errnum)
 {
-	pam_handle_t *handle = fetch_handle(env,obj);
+	struct native_data *data = get_data(env,obj);
+	pam_handle_t *handle = data->pamhandle;
 	const char *error = call_pam_strerror(handle,errnum);
 	return env->NewStringUTF(error);
 }
--- src/com/blueprintit/security/pam/Pam.java
+++ src/com/blueprintit/security/pam/Pam.java
@@ -7,127 +7,117 @@ public class Pam
 {
 	/* ----------------- The Linux-PAM return values ------------------ */
 
-	public static int PAM_SUCCESS = 0;           			/* Successful function return */
-	public static int PAM_OPEN_ERR = 1;          			/* dlopen() failure when dynamically */
-	                                									/* loading a service module */
-	public static int PAM_SYMBOL_ERR = 2;        			/* Symbol not found */
-	public static int PAM_SERVICE_ERR = 3;       			/* Error in service module */
-	public static int PAM_SYSTEM_ERR = 4;        			/* System error */
-	public static int PAM_BUF_ERR = 5;           			/* Memory buffer error */
-	public static int PAM_PERM_DENIED = 6;       			/* Permission denied */
-	public static int PAM_AUTH_ERR = 7;          			/* Authentication failure */
-	public static int PAM_CRED_INSUFFICIENT = 8; 			/* Can not access authentication data */
-	                                									/* due to insufficient credentials */
-	public static int PAM_AUTHINFO_UNAVAIL = 9;  			/* Underlying authentication service */
-	                                									/* can not retrieve authenticaiton */
-	                                									/* information  */
-	public static int PAM_USER_UNKNOWN = 10;     			/* User not known to the underlying */
-	                                									/* authenticaiton module */
-	public static int PAM_MAXTRIES = 11;         			/* An authentication service has */
-	                                									/* maintained a retry count which has */
-	                                									/* been reached.  No further retries */
-	                                									/* should be attempted */
-	public static int PAM_NEW_AUTHTOK_REQD = 12; 			/* New authentication token required. */
-	                                									/* This is normally returned if the */
-	                                									/* machine security policies require */
-	                                									/* that the password should be changed */
-	                                									/* beccause the password is NULL or it */
-	                                									/* has aged */
-	public static int PAM_ACCT_EXPIRED = 13;     			/* User account has expired */
-	public static int PAM_SESSION_ERR = 14;      			/* Can not make/remove an entry for */
-	                                									/* the specified session */
-	public static int PAM_CRED_UNAVAIL = 15;     			/* Underlying authentication service */
-	                                									/* can not retrieve user credentials */
-	                                									/* unavailable */
-	public static int PAM_CRED_EXPIRED = 16;     			/* User credentials expired */
-	public static int PAM_CRED_ERR = 17;         			/* Failure setting user credentials */
-	public static int PAM_NO_MODULE_DATA = 18;   			/* No module specific data is present */
-	public static int PAM_CONV_ERR = 19;         			/* Conversation error */
-	public static int PAM_AUTHTOK_ERR = 20;      			/* Authentication token manipulation error */
-	public static int PAM_AUTHTOK_RECOVER_ERR = 21; 	/* Authentication information */
-	                                   								/* cannot be recovered */
-	public static int PAM_AUTHTOK_LOCK_BUSY = 22;   	/* Authentication token lock busy */
-	public static int PAM_AUTHTOK_DISABLE_AGING = 23; /* Authentication token aging disabled */
-	public static int PAM_TRY_AGAIN = 24;        			/* Preliminary check by password service */
-	public static int PAM_IGNORE = 25;           			/* Ingore underlying account module */
-	                                									/* regardless of whether the control */
-	                                									/* flag is required, optional, or sufficient */
-	public static int PAM_ABORT = 26;            			/* Critical error (?module fail now request) */
-	public static int PAM_AUTHTOK_EXPIRED  = 27; 			/* user's authentication token has expired */
-	public static int PAM_MODULE_UNKNOWN   = 28; 			/* module is not known */
+	public static final int PAM_SUCCESS = 0;           			/* Successful function return */
+	public static final int PAM_OPEN_ERR = 1;          			/* dlopen() failure when dynamically */
+	                                												/* loading a service module */
+	public static final int PAM_SYMBOL_ERR = 2;        			/* Symbol not found */
+	public static final int PAM_SERVICE_ERR = 3;       			/* Error in service module */
+	public static final int PAM_SYSTEM_ERR = 4;        			/* System error */
+	public static final int PAM_BUF_ERR = 5;           			/* Memory buffer error */
+	public static final int PAM_PERM_DENIED = 6;       			/* Permission denied */
+	public static final int PAM_AUTH_ERR = 7;          			/* Authentication failure */
+	public static final int PAM_CRED_INSUFFICIENT = 8; 			/* Can not access authentication data */
+	                                												/* due to insufficient credentials */
+	public static final int PAM_AUTHINFO_UNAVAIL = 9;  			/* Underlying authentication service */
+	                                												/* can not retrieve authenticaiton */
+	                                												/* information  */
+	public static final int PAM_USER_UNKNOWN = 10;     			/* User not known to the underlying */
+	                                												/* authenticaiton module */
+	public static final int PAM_MAXTRIES = 11;         			/* An authentication service has */
+	                                												/* maintained a retry count which has */
+	                                												/* been reached.  No further retries */
+	                                												/* should be attempted */
+	public static final int PAM_NEW_AUTHTOK_REQD = 12; 			/* New authentication token required. */
+	                                												/* This is normally returned if the */
+	                                												/* machine security policies require */
+	                                												/* that the password should be changed */
+	                                												/* beccause the password is NULL or it */
+	                                												/* has aged */
+	public static final int PAM_ACCT_EXPIRED = 13;     			/* User account has expired */
+	public static final int PAM_SESSION_ERR = 14;      			/* Can not make/remove an entry for */
+	                                												/* the specified session */
+	public static final int PAM_CRED_UNAVAIL = 15;     			/* Underlying authentication service */
+	                                												/* can not retrieve user credentials */
+	                                												/* unavailable */
+	public static final int PAM_CRED_EXPIRED = 16;     			/* User credentials expired */
+	public static final int PAM_CRED_ERR = 17;         			/* Failure setting user credentials */
+	public static final int PAM_NO_MODULE_DATA = 18;   			/* No module specific data is present */
+	public static final int PAM_CONV_ERR = 19;         			/* Conversation error */
+	public static final int PAM_AUTHTOK_ERR = 20;      			/* Authentication token manipulation error */
+	public static final int PAM_AUTHTOK_RECOVER_ERR = 21; 	/* Authentication information */
+	                                   											/* cannot be recovered */
+	public static final int PAM_AUTHTOK_LOCK_BUSY = 22;   	/* Authentication token lock busy */
+	public static final int PAM_AUTHTOK_DISABLE_AGING = 23; /* Authentication token aging disabled */
+	public static final int PAM_TRY_AGAIN = 24;        			/* Preliminary check by password service */
+	public static final int PAM_IGNORE = 25;           			/* Ingore underlying account module */
+	                                												/* regardless of whether the control */
+	                                												/* flag is required, optional, or sufficient */
+	public static final int PAM_ABORT = 26;            			/* Critical error (?module fail now request) */
+	public static final int PAM_AUTHTOK_EXPIRED  = 27; 			/* user's authentication token has expired */
+	public static final int PAM_MODULE_UNKNOWN   = 28; 			/* module is not known */
 
-	public static int PAM_BAD_ITEM         = 29; 			/* Bad item passed to pam_*_item() */
-	public static int PAM_CONV_AGAIN       = 30; 			/* conversation function is event driven
-	                                     								and data is not available yet */
-	public static int PAM_INCOMPLETE       = 31; 			/* please call this function again to
-	                                   									complete authentication stack. Before
-	                                   									calling again, verify that conversation
-	                                   									is completed */
+	public static final int PAM_BAD_ITEM         = 29; 			/* Bad item passed to pam_*_item() */
+	public static final int PAM_CONV_AGAIN       = 30; 			/* conversation function is event driven
+	                                     											and data is not available yet */
+	public static final int PAM_INCOMPLETE       = 31; 			/* please call this function again to
+	                                   												complete authentication stack. Before
+	                                   												calling again, verify that conversation
+	                                   												is completed */
 
 	/* ---------------------- The Linux-PAM flags -------------------- */
 
 	/* Authentication service should not generate any messages */
-	public static int PAM_SILENT                      =0x8000;
+	public static final int PAM_SILENT                      =0x8000;
 
 	/* Note: these flags are used by pam_authenticate{,_secondary}() */
 
 	/* The authentication service should return PAM_AUTH_ERROR if the
 	 * user has a null authentication token */
-	public static int PAM_DISALLOW_NULL_AUTHTOK       =0x0001;
+	public static final int PAM_DISALLOW_NULL_AUTHTOK       =0x0001;
 
 	/* Note: these flags are used for pam_setcred() */
 
 	/* Set user credentials for an authentication service */
-	public static int PAM_ESTABLISH_CRED              =0x0002;
+	public static final int PAM_ESTABLISH_CRED              =0x0002;
 
 	/* Delete user credentials associated with an authentication service */
-	public static int PAM_DELETE_CRED                 =0x0004;
+	public static final int PAM_DELETE_CRED                 =0x0004;
 
 	/* Reinitialize user credentials */
-	public static int PAM_REINITIALIZE_CRED           =0x0008;
+	public static final int PAM_REINITIALIZE_CRED           =0x0008;
 
 	/* Extend lifetime of user credentials */
-	public static int PAM_REFRESH_CRED                =0x0010;
+	public static final int PAM_REFRESH_CRED                =0x0010;
 
 	/* Note: these flags are used by pam_chauthtok */
 
 	/* The password service should only update those passwords that have
 	 * aged.  If this flag is not passed, the password service should
 	 * update all passwords. */
-	public static int PAM_CHANGE_EXPIRED_AUTHTOK      =0x0020;
+	public static final int PAM_CHANGE_EXPIRED_AUTHTOK      =0x0020;
 
 	/* ------------------ The Linux-PAM item types ------------------- */
 
 	/* these defines are used by pam_set_item() and pam_get_item() */
 
-	//public static int PAM_SERVICE       = 1;    /* The service name */
-	//public static int PAM_USER          = 2;    /* The user name */
-	//public static int PAM_TTY           = 3;    /* The tty name */
-	//public static int PAM_RHOST         = 4;    /* The remote host name */
-	//public static int PAM_CONV          = 5;    /* The pam_conv structure */
+	//public static final int PAM_SERVICE       = 1;    /* The service name */
+	//public static final int PAM_USER          = 2;    /* The user name */
+	//public static final int PAM_TTY           = 3;    /* The tty name */
+	//public static final int PAM_RHOST         = 4;    /* The remote host name */
+	//public static final int PAM_CONV          = 5;    /* The pam_conv structure */
 
-	//public static int PAM_RUSER         = 8;    /* The remote user name */
-	//public static int PAM_USER_PROMPT   = 9;    /* the prompt for getting a username */
-	//public static int PAM_FAIL_DELAY    = 10;   /* app supplied function to override failure
-	//                                   						delays */
+	//public static final int PAM_RUSER         = 8;    /* The remote user name */
+	//public static final int PAM_USER_PROMPT   = 9;    /* the prompt for getting a username */
+	//public static final int PAM_FAIL_DELAY    = 10;   /* app supplied function to override failure
+	//                                   									delays */
 	
-	private static int uid;
-	
-	static
-	{
-		uid=1;
-		System.loadLibrary("pamcalls");
-	}
-	
-	private int myid;
 	private PamCallback callback;
 	private int status;
 	private boolean valid = false;
+	private byte[] nativeData;
 	
 	public Pam(String service, String user, PamCallback callback)
 	{
-		myid = uid;
-		uid++;
 		this.callback=callback;
 		status=call_pam_start(service,user,callback);
 		if (status!=PAM_SUCCESS)
@@ -137,15 +127,25 @@ public class Pam
 		valid=true;
 	}
 	
-	private int getID()
+	private void setNativeData(byte[] data)
 	{
-		return myid;
+		nativeData=data;
+	}
+	
+	private byte[] getNativeData()
+	{
+		return nativeData;
 	}
 	
 	public void finalize() throws Throwable
 	{
 		if (valid)
 			pam_end(getStatus());
+	}
+	
+	static
+	{
+		System.loadLibrary("pamcalls");
 	}
 	
 	private native int call_pam_start(String service, String user, PamCallback callback);
